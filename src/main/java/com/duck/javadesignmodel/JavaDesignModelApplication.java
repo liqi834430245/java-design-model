b5294476d897de6e7fb3b3d9ab34ef4ac429d56a
kvien <834430245@qq.com>
2019-07-16 23:12:47 +0800

    new java design model by ideal

---
+++ src/main/java/com/duck/javadesignmodel/JavaDesignModelApplication.java
@@ -0,0 +1,13 @@
+package com.duck.javadesignmodel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaDesignModelApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaDesignModelApplication.class, args);
+    }
+
+}
